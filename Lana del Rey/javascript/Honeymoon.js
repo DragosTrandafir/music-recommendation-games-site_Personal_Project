76ea2b37281fc05edf8bef1ecf194d0f307882b3
DragosTrandafiri <dragostrandafir333@gmail.com>
2025-02-17 21:03:23 +0200

    added some js comments in honeymoon.js and changed elem to wheel

--- Lana del Rey/javascript/Honeymoon.js
+++ Lana del Rey/javascript/Honeymoon.js
@@ -1,28 +1,30 @@
 
-function randomiseMove(){
-  let elem = document.querySelector('.js-wheel');
-  let move=Math.random();
-  move = Math.floor(Math.random()*13);
-  elem.style.animation=`wheel-animation${move+1} 10s cubic-bezier(0.19, 1, 0.22, 1) 1`;
-  const randomisedText = document.querySelector(`.song${move+1}`).innerText;
-  setTimeout(()=>{
+function randomiseMove() {
+  let wheel = document.querySelector('.js-wheel'); //select the wheel element
+  let move = Math.floor(Math.random() * 13);
+  wheel.style.animation = `wheel-animation${move + 1} 10s cubic-bezier(0.19, 1, 0.22, 1) 1`;
+  const randomisedText = document.querySelector(`.song${move + 1}`).innerText; //randomise an animation and a song, both corresponding to the same index
+  setTimeout(() => {
     alert(`Go listen to ${randomisedText}!`);
-    listenNow(randomisedText);}
-    ,10000);
+    listenNow(randomisedText);
+  }
+    , 10000);
 }
 
+// call randomise only when we click the spin-button
 document.querySelector('.js-spin-button')
-.addEventListener('click', ()=>{
-  let wheelSelector = document.querySelector('.wheel');
-  wheelSelector.classList.add("js-wheel");
-  randomiseMove();
-  let elem = document.querySelector('.js-spin-button');
-  elem.style.backgroundColor='red';
-})
+  .addEventListener('click', () => {
+    let wheelSelector = document.querySelector('.wheel');
+    wheelSelector.classList.add("js-wheel");
+    randomiseMove();
+    let wheel = document.querySelector('.js-spin-button');
+    wheel.style.backgroundColor = 'red';
+  })
 
-function listenNow(randomisedText){
-   document.querySelector('.js-go-listen')
-   .innerHTML=`
+// open a spotify link by including some inner html 
+function listenNow(randomisedText) {
+  document.querySelector('.js-go-listen')
+    .innerHTML = `
     <p><a href="https://open.spotify.com/album/2DpEBrjCur1ythIZ10gJWw" target="_blank" class="listen-now-animation">Listen to ${randomisedText} here!</p>
    `;
 }
